d7dd99adff2b6ec8ab1452eb8ce6f05e8cc102f0
Ed Page <eopage@gmail.com>
2021-08-03 18:59:02 -0500

    fix: Decouple UX from core API
    This increases the API surface area, making it more likely we'll break
    it.  Supporting `AlwaysAnsi` is an example of such a case.

--- crates/clap/src/lib.rs
+++ crates/clap/src/lib.rs
@@ -12,27 +12,7 @@
 //! }
 //! ```
 
-pub use concolor_control::ColorChoice;
-
-#[derive(Copy, Clone, Default, Debug, PartialEq, Eq, structopt::StructOpt)]
-pub struct Color {
-    /// Controls when to use color.
-    #[structopt(long, default_value = "auto", value_name = "WHEN")]
-    color: ColorChoice,
-}
-
-impl Color {
-    /// Set the user selection on `concolor_control`
-    pub fn apply(&self) {
-        concolor_control::set(self.when());
-    }
-
-    /// Get the user's selection
-    pub fn when(&self) -> ColorChoice {
-        self.color
-    }
-}
-
+/// Get color choice for initializing the `clap::App`
 pub fn color_choice() -> structopt::clap::AppSettings {
     let color = concolor_control::get(concolor_control::Stream::Either);
     if color.ansi_color() {
@@ -41,3 +21,83 @@ pub fn color_choice() -> structopt::clap::AppSettings {
         structopt::clap::AppSettings::ColorNever
     }
 }
+
+#[derive(Copy, Clone, Default, Debug, PartialEq, Eq, structopt::StructOpt)]
+pub struct Color {
+    /// Controls when to use color.
+    #[structopt(long, default_value = "auto", value_name = "WHEN")]
+    pub color: ColorChoice,
+}
+
+impl Color {
+    /// Set the user selection on `concolor_control`
+    pub fn apply(&self) {
+        concolor_control::set(self.to_control());
+    }
+
+    /// Get the user's selection
+    pub fn to_control(&self) -> concolor_control::ColorChoice {
+        match self.color {
+            ColorChoice::Auto => concolor_control::ColorChoice::Auto,
+            ColorChoice::Always => concolor_control::ColorChoice::Always,
+            ColorChoice::Never => concolor_control::ColorChoice::Never,
+        }
+    }
+}
+
+/// Selection for when to color output
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum ColorChoice {
+    Auto,
+    Always,
+    Never,
+}
+
+impl ColorChoice {
+    /// All color choices
+    pub const fn choices() -> &'static [Self] {
+        &[ColorChoice::Auto, ColorChoice::Always, ColorChoice::Never]
+    }
+
+    /// All color choice argument values
+    pub const fn values() -> &'static [&'static str] {
+        &[AUTO, ALWAYS, NEVER]
+    }
+
+    pub const fn value(self) -> &'static str {
+        match self {
+            Self::Auto => AUTO,
+            Self::Always => ALWAYS,
+            Self::Never => NEVER,
+        }
+    }
+}
+
+impl Default for ColorChoice {
+    fn default() -> Self {
+        Self::Auto
+    }
+}
+
+impl core::fmt::Display for ColorChoice {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        self.value().fmt(f)
+    }
+}
+
+impl std::str::FromStr for ColorChoice {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            AUTO => Ok(Self::Auto),
+            ALWAYS => Ok(Self::Always),
+            NEVER => Ok(Self::Never),
+            other => Err(format!("Unknown color choice '{}'", other)),
+        }
+    }
+}
+
+const AUTO: &str = "auto";
+const ALWAYS: &str = "always";
+const NEVER: &str = "never";
